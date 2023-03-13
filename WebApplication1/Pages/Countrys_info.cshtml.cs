0d1d68ba4b27517db741ac463040f64ffba5eb5b
Pavlo Brychka <brychkapavlo.work@gmail.com>
2023-03-13 21:05:26 +0200

    Add project files.

---
+++ WebApplication1/Pages/Countrys_info.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class Countrys_infoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
